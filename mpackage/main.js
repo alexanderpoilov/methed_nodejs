7984c85ecd4bf7e1a86d85e1b43a5e6fd046d1b5
Alexander Poilov <alexanderpoilov@Alexanders-MacBook-Air.local>
2023-08-05 03:35:04 +0300

    lesson02 hw in process

--- mpackage/main.js
+++ mpackage/main.js
@@ -1,9 +1,4 @@
-const createUUID = () => {
-  return 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      const r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-      return v.toString(16);
-  });
-}
+const { v4: uuidv4 } = require('uuid');
 
 const setFirstLetterCapital = word => word ? word[0].toUpperCase() + word.toLowerCase().slice(1)
   : 'не указано';
@@ -17,7 +12,7 @@ const createUserTemplate = ({name, dateBirth, purpose}) => {
   const lastName = setFirstLetterCapital(name.split(' ')[1]);
 
   return {
-    id: createUUID(),
+    id: uuidv4(),
     firstname: firstName,
     lastname: lastName,
     dateBirth: dateBirth,
